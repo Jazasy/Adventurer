ac6a2c3022a7ae5b03194231371ca0eda708cb85
jazasy <janoserdosy1@gmail.com>
2024-08-04 19:05:27 +0200

    set up home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,17 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import "./App.css"
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
 
 axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.withCredentials = true;
 
 export default function App() {
-	const [testData, setTestData] = useState();
-	useEffect(() => {
-		axios.get("/test").then((response) => setTestData(response.data));
-	}, []);
 	return (
 		<>
-			<CssBaseline />
-			<div className = "container">
-                <header className="head"></header>
-                <main className="main"></main>
-                <footer className="foot"></footer>
-            </div>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
 		</>
 	);
 }
