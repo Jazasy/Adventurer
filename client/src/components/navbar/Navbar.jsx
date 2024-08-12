414410dda26640dca49b67c57a6880295a84fc86
Jazasy <134019402+Jazasy@users.noreply.github.com>
2024-08-12 21:02:44 +0200

    started making navbar

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import "./Navbar.css"
+
+export default function Navbar(){
+    return (
+        <div className="navbar">
+            <button>Register</button>
+            <button>Login</button>
+        </div>
+    )
+}
