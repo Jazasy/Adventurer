2557f5199dd877052009a68f8206cfa752669158
jazasy <janoserdosy1@gmail.com>
2024-09-01 16:59:51 +0200

    deleting navbar, it was just a test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import Navbar from "./components/navbar/Navbar";
 
 axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.withCredentials = true;
@@ -10,7 +9,6 @@ axios.defaults.withCredentials = true;
 export default function App() {
 	return (
 		<>
-			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 			</Routes>
