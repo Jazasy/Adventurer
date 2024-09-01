58aae293cd80d40671f688b096b8f6162ad05264
jazasy <janoserdosy1@gmail.com>
2024-09-01 22:45:59 +0200

    making adventures grid

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,11 @@
-export default function Home(){
-    return (
-        <h1>home</h1>
-    )
+import AdventureBoard from "../components/AdventureBoard";
+import "./Home.css";
+
+export default function Home() {
+	return (
+		<>
+			<h1>home</h1>
+			<AdventureBoard />
+		</>
+	);
 }
