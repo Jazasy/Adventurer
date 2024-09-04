705ee54808eaeebec2ddca2c1c6621a6bcf6e715
jazasy <janoserdosy1@gmail.com>
2024-09-04 18:36:10 +0200

    work on navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import "./App.css";
 import Home from "./pages/Home";
 import Show from "./pages/Show";
 import Navbar from "./components/Navbar/Navbar";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.withCredentials = true;
@@ -15,6 +17,8 @@ export default function App() {
 			<Routes>
 				<Route path="/home" element={<Home />} />
 				<Route path="/adventures/:id" element={<Show />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
 		</>
 	);
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,41 @@
 import AppLogo from "../icons/AppLogo";
 import "./Navbar.css";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function Navbar() {
+	const navigate = useNavigate();
+	const currentPath = useLocation().pathname;
+	const handleClick = (event) => {
+		const path = event.target.getAttribute("path");
+		navigate(`/${path}`);
+	};
 	return (
 		<div className="navbar">
-			<div className="nav-logo"><AppLogo /></div>
+			<div className="nav-logo">
+				<AppLogo />
+			</div>
 			<ul className="nav-items">
-				<li className="nav-item">Home</li>
-				<li className="nav-item">Sign up</li>
-				<li className="nav-item">Login</li>
+				<li
+					className={currentPath === "/home" ? "nav-item selected" : "nav-item"}
+					onClick={handleClick}
+					path="home"
+				>
+					Home
+				</li>
+				<li
+					className={currentPath === "/register" ? "nav-item selected" : "nav-item"}
+					onClick={handleClick}
+					path="register"
+				>
+					Sign up
+				</li>
+				<li
+					className={currentPath === "/login" ? "nav-item selected" : "nav-item"}
+					onClick={handleClick}
+					path="login"
+				>
+					Login
+				</li>
 			</ul>
 		</div>
 	);
