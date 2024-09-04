b743019becd3e0213a3ac614661c563615e5eb07
jazasy <janoserdosy1@gmail.com>
2024-09-04 23:53:57 +0200

    dopdown navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,29 @@
 import AppLogo from "../icons/AppLogo";
 import "./Navbar.css";
 import { useNavigate, useLocation } from "react-router-dom";
+import { useState } from "react";
 
 export default function Navbar() {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 	const navigate = useNavigate();
 	const currentPath = useLocation().pathname;
 	const handleClick = (event) => {
 		const path = event.target.getAttribute("path");
 		navigate(`/${path}`);
+		setIsMenuOpen(false);
 	};
+	const handleMenuClick = () => {
+		setIsMenuOpen(oldIsMenuOpen => !oldIsMenuOpen);
+	}
 	return (
 		<div className="navbar">
-			<div className="nav-logo">
-				<AppLogo />
+			<div className="nav-head">
+				<div className="nav-logo">
+					<AppLogo />
+				</div>
+				<i className="fa-solid fa-bars" onClick={handleMenuClick}></i>
 			</div>
-			<ul className="nav-items">
+			<ul className={isMenuOpen ? "nav-items nav-items-open" : "nav-items"}>
 				<li
 					className={currentPath === "/home" ? "nav-item selected" : "nav-item"}
 					onClick={handleClick}
@@ -23,14 +32,18 @@ export default function Navbar() {
 					Home
 				</li>
 				<li
-					className={currentPath === "/register" ? "nav-item selected" : "nav-item"}
+					className={
+						currentPath === "/register" ? "nav-item selected" : "nav-item"
+					}
 					onClick={handleClick}
 					path="register"
 				>
 					Sign up
 				</li>
 				<li
-					className={currentPath === "/login" ? "nav-item selected" : "nav-item"}
+					className={
+						currentPath === "/login" ? "nav-item selected" : "nav-item"
+					}
 					onClick={handleClick}
 					path="login"
 				>
