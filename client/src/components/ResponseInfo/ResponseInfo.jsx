49e97ec06877f42450e2468c474c7df8f7a58538
jazasy <janoserdosy1@gmail.com>
2024-09-06 21:05:44 +0200

    basic timer

--- client/src/components/ResponseInfo/ResponseInfo.jsx
+++ client/src/components/ResponseInfo/ResponseInfo.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import "./ResponseInfo.css";
+import { useAdventures } from "../../contexts/useAdventures";
 
-export default function ResponseInfo({info}) {
+export default function ResponseInfo({ info }) {
+	const { resInfos, setResInfos } = useAdventures();
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setResInfos((oldResInfos) =>
+				oldResInfos.filter((resInfo) => resInfo !== info)
+			);
+		}, 5000);
+		return () => clearTimeout(timer);
+	}, []);
+
 	return (
 		<div className="response-info">
 			<h1>{info}</h1>
